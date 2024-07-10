bfe2edfe2a42fd5eaa7e6b77a020a11b505c7889
PhidLarkson <phiddyconcept@gmail.com>
2024-07-10 01:03:57 +0000

    chore: Update audio processing libraries and requirements

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
 from flask import Flask, request, jsonify
 from faster_whisper import WhisperModel
-from pydub import AudioSegment
-from pydub.playback import play
-import sounddevice as sd
-from scipy.io.wavfile import write
 import os
 
 app = Flask(__name__)
 
 model_size = "distil-large-v3"
-fs = 44100  # Sample rate
-seconds = 30  # Duration of recording
 
-@app.route('/transcribe', methods=['POST'])
+# @app.route('/transcribe', methods=['POST'])
 def transcribe():
-    # Get the audio file from the request
-    audio_file = request.files['audio']
-    audio_file.save('temp.wav')
+    # Get the audio data from the request
+    audio_data = request.files['audio'].read()
+
+    # Save the audio data to a temporary file
+    with open('temp.wav', 'wb') as f:
+        f.write(audio_data)
+
     # Transcribe the audio
-    output = transcribe_audio()
+    output = transcribe_audio('temp.wav')
 
     # Remove the temporary audio file
     os.remove('temp.wav')
+
+    # Return the transcription as JSON
     return jsonify({'transcription': output})
 
-def transcribe_audio():
+def transcribe_audio(audio_file):
     output = ""
-    # Record audio
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2, dtype='int16')
-    sd.wait()  # Wait until recording is finished
-    print("Audio recording complete , Play Audio")
-
-    # Save as WAV file
-    write('temp.wav', fs, myrecording) 
-
     model = WhisperModel(model_size, device="cpu", compute_type="int8")
-    segments, info = model.transcribe("temp.wav", beam_size=5, language="en", condition_on_previous_text=False)
+    segments, info = model.transcribe(audio_file, beam_size=5, language="en", condition_on_previous_text=False)
     for segment in segments:
         output += segment.text
-
     return output
 
 if __name__ == '__main__':
